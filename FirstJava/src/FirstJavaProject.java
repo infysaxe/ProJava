5c4d8a0744d91efbf1e37007aaf04bbc8fa4005c
sadmin <sadmin@192.168.1.62>
2018-10-21 13:53:20 +0200

    First commit from FirstJava project to ProJava

---
+++ FirstJava/src/FirstJavaProject.java
@@ -0,0 +1,12 @@
+
+public class FirstJavaProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		Calculator cAdd = new Calculator ();
+		System.out.println("Sum of " + a + " and " + b + " is " + cAdd.add(a,b));
+	}
+
+}
